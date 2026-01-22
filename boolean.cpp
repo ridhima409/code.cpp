716b2f91a5adce28a6d8efa099a17596b415a816
Ridhima dubey <ridhimadubey05june@gmail.com>
2026-01-22 16:52:06 +0000

    container config done

---
+++ boolean.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age = 25;
+    char grade = 'a';
+    float PI = 3.14f;
+    bool isSafe = true;
+    cout<<isSafe <<endl;
+    return 0;
+}
