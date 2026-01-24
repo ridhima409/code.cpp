bd033932d1e726c1c87d96fd3610c24639fe3c57
Ridhima dubey <ridhimadubey05june@gmail.com>
2026-01-24 15:39:07 +0000

    container config done

---
+++ marks.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter marks";
+    cin>>marks;
+    if(marks>=90){
+        cout<<"A\n";
+    }else if(marks>=80&& marks<90){
+        cout<<"B\n";
+    }else{
+        cout<<"c\n";
+    }
+    return 0;
+
+
+    }
