2c467be61fa43547dd48bfa996a66a21b7a7fcde
Ridhima dubey <ridhimadubey05june@gmail.com>
2026-01-24 15:30:20 +0000

    container config done

---
+++ number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    if(n%2==0){
+        cout<<"even\n";
+    }else{
+        cout<<"odd\n";
+    }
+    return 0;
+}
