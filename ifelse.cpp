6b8f4c6335a5e7fe5c82f80d1a2ffd2d359aa01c
Ridhima dubey <ridhimadubey05june@gmail.com>
2026-01-24 15:21:55 +0000

    container config done

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter the age";
+    cin>>age;
+    if(age>=18){
+        cout<<"you can vote\n";
+
+    }else{
+        cout<<"you can't vote\n";
+    }
+    return 0;
+}
---
+++ negativeno.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 50;
+    if(n>=0){
+        cout<<"n is negative number\n";
+    }else{
+        cout<<"n is postive number\n";
+    }
+    return 0;
+
+}
