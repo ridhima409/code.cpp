f3073bc50d4b0b87fc65d93bc2611ae03e192ed0
Ridhima dubey <ridhimadubey05june@gmail.com>
2026-01-29 15:48:38 +0000

    container config done

---
+++ Loop/for2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    int sum = 0;
+    for(int i=1;i<=n;i++ ){
+        sum +=1;
+    }
+    cout<<"sum="<<sum<<endl;
+    return 0;
+}
