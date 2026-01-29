b7f0bf40ca80647aceae1c69d3a70630d113959e
Ridhima dubey <ridhimadubey05june@gmail.com>
2026-01-29 15:30:57 +0000

    container config done

---
+++ Loop/while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count = 1;
+    while(count<=45){
+        cout<<count<<"";
+        count ++;
+    }
+    cout<<endl;
+    return 0;
+}
+
+    
+  
+
---
+++ Ternaryoperator.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = -45;
+    cout<<(n>=0?"postive":"negative")<<endl;
+    return 0;
+}
